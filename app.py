e4c5f8c5d12a0be9537e300fcf0410270c83808e
svorogaze <68436858+svorogaze@users.noreply.github.com>
2020-12-20 19:03:13 +0300

    update

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ file = open('grades.txt', 'r', encoding='utf-8')
 for i in file.readlines():
     splitted = i.split()
     dict_grades[splitted[0]] = splitted[1:]
+sr_math = round(sum(dict_grades['математика']) / len(dict_grades['математика']), 1)
+sr_russian = round(sum(dict_grades['математика']) / len(dict_grades['математика']), 1)
 
 
 @app.route('/')
 def home():
-    return render_template('proekt.html', gd=dict_grades)
+    return render_template('proekt.html', gd=dict_grades, sr_m=sr_math, sr_r=sr_russian)
 
 
 if __name__ == '__main__':
